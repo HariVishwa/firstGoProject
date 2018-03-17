1f5f03f8cdb1c6bd16374634f53d49371c8429c8
harivishwa <harivishwa27012002@gmail.com>
2018-03-17 23:00:26 +0530

    My first commit in github

---
+++ addition.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x, y int
+	fmt.Print("Give first number:")
+	fmt.Scan(&x)
+	fmt.Print("Give second number:")
+	fmt.Scan(&y)
+
+	z := add(x, y)
+	fmt.Printf("The addition is:%d", z)
+}
+
+// add mperforms addition of two numbers.
+func add(x, y int) int {
+	return x + y
+}
